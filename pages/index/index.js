044d28b4962c1fec4ffb995ddd3aaf6f3af45b74
Besam-juajua <1695744322@qq.com>
2019-03-15 00:17:49 +0800

    finish some pages

---
+++ pages/index/index.js
@@ -0,0 +1,11 @@
+const app = getApp();
+Page({
+  data: {
+    swiperList: [
+      "https://dummyimage.com/357x189/000/fff",
+      "https://dummyimage.com/357x189/000/fff",
+      "https://dummyimage.com/357x189/000/fff",
+      "https://dummyimage.com/357x189/000/fff"
+    ]
+  }
+})
